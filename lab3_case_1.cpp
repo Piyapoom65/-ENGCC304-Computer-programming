678e9f5179a0c6151eb93cf8c1083aa16fae0857
KIFAKER86H <107609463+KIFAKER86H@users.noreply.github.com>
2022-07-17 03:49:34 +0700

    Update lab3_case_1.cpp

--- lab3_case_1.cpp
+++ lab3_case_1.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int a;
-    float b;
-    char c;
-    printf("please input value: ");
-    scanf("%d %f %c",&a, &b, &c);
-    printf("%d %f %c",a,b,c);
-    return(0);
+    int a ;
+    float b ;
+    char c ;
+    printf("please input value: ") ;
+    scanf("%d %f %c",&a, &b, &c) ;
+    printf("%d %f %c",a,b,c) ;
+    return( 0 ) ;
 }
