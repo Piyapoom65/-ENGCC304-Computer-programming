ba6fedad086fbdc2da328282bc41a74268fe5d89
KIFAKER86H <107609463+KIFAKER86H@users.noreply.github.com>
2022-07-19 11:01:01 +0700

    Add files via upload

---
+++ lab3_case_1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char c;
+    printf("please input value: ");
+    scanf("%d %f %c",&a, &b, &c);
+    printf("%d %f %c",a,b,c);
+    return(0);
+}
