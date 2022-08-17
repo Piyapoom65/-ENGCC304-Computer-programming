52ccec3a3d419da1059160267d666d6f4e0e7e28
KIFAKER86H <107609463+KIFAKER86H@users.noreply.github.com>
2022-08-18 01:25:00 +0700

    Add files via upload

---
+++ Lab7/lab_7 Edit_downstruc.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int num ; 
+    //ค่าเริ่มต้นที่จะนับจนถึง0
+    int j ; 
+    //ค่าที่ใช้ในการเช็คค่าจำนวนเฉพาะ
+    int rep = 0 ; 
+    //จำนวนครั้งที่ค่าnum/ค่าjได้ลงตัว
+    printf( "Input : " ) ;
+    scanf( "%d", &num ) ;
+    while( num >= 2 )
+    {
+        j = num ;
+        while ( j >= 2 )
+        {
+            if( rep > 1 ) 
+            {
+                break;
+            }
+            if ( num%j == 0 )
+            {
+                rep++ ;
+            }
+            j-- ;
+        }
+        if ( rep <= 1 )
+        {
+            printf( "%d ", num ) ;
+        }
+        rep = 0 ;
+        num-- ;
+    }
+}
